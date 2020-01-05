79a5eabea424c397617627df9b4dd8c0b796bb26
LeonhardPassini <leonhard.passini03@htl-salzburg.ac.at>
2020-01-05 10:37:36 +0100

    add helloWorldHome in MainScene

--- Assets/MyGame/Scripts/HelloWorldHome.cs
+++ Assets/MyGame/Scripts/HelloWorldHome.cs
@@ -1,6 +1,6 @@
 ﻿using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class HelloWorldHome : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
